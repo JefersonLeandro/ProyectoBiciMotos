25f2e604e35fc96281d3def5d78515812986670f
Jeferson Leandro <jefersonleandrorodriguez@gmail.com>
2023-12-21 18:31:20 -0500

    la plantilla de la vista de la factura esta terminada con estilos superficiales

--- app/Http/Controllers/controladorFactura.php
+++ app/Http/Controllers/controladorFactura.php
@@ -8,6 +8,18 @@ class controladorFactura extends Controller
 {
     public function index(){
 
+        //terminar de darle estilos a la pagina 
+        //-actualizar todas las cantidades de los usuarios donde las cantidades de los productos fueron compradas 
+        //o generaron la factura llegandole a restar el stock el linea de cada producto sobre el que fue comprado
+        //-generar un subtotal para ese usuario con sus cantidades y precios , \ usar el que ya esta. 
+        //listar todos los carritos de un usuario y insertarlos en la tabla detalles para la factura 
+        //y traer el id de esa factura para relacionarlos a cada detalle y eliminar esos carritos que ya fueron comprados 
+        // hacer si tanto complique,en la parte de indexar si no hay ningun producto en carrito y ademas mirar si no tiene ningun detalle 
+        // , no hacer nada pero si tiene un detalle y el usuario tiene carrito vacio fue por que compro y de ser asi mostrarlos 
+        // una sola vez en la vista carrito con un array y ya porque tambien o sino solo elimimarlos y ya y verificar en amazon como hacen si los eliminan o los dejan.
+        
+
+
         return view("factura");
 
     }
--- resources/views/factura.blade.php
+++ resources/views/factura.blade.php
@@ -1,84 +1,151 @@
+ 
 <x-layouts.plantillaPrincipal title="factura" meta-description=" esta es la descripcion de la factura">
 
 
-    <main class=" border border-solid border-black flex items-center justify-center bg-sky-300 w-full" style="height:105vh;">
+    <main class="  flex items-center justify-center w-full" style="height:105vh;">
         
-        <div class=" w-6/12 bg-amber-400 " style="height: 90vh; ">
+        <div class=" w-6/12 border border-solid border-black " style="height: auto; ">
 
-            <div class="flex  justify-between items-center bg-slate-200 pl-2 pr-2 h-12">
+            <div class="flex justify-between items-center bg-slate-200 pl-2 pr-2 h-12">
                 <h1 class="text-2xl ">BiciMotos </h1>
                 <h2 class="text-xl">Factura de venta</h2>
                
             </div>
-            <div class="w-full h-  flex flex-col gap-4 bg-green-400 p-4 pt-7  " style="height: 85%;">
-                <div class="flex justify-between">
+            <div class="w-full h-96  flex flex-col gap-4  p-4 pt-7  " style="height: 93%;">
+                <div class="flex justify-between border-solid  border-b-2 text-base border-b-slate-900">
                     <p>Camilo Suarez Martinez</p>
                     <script>
                         // Date date =new Date();
                         // console.log(date);
                     </script>
-
-                    <p>fecha : 19/12/2023 fija</p>
+                    <div class="flex gap-3">
+                        <p>fecha : 19/12/2023 </p>
+                        <p>home</p>
+                    </div>
+                    
                 </div>
-                <div class="bg-red-400 h-3/4">
+                <div class=" h-full">
                      <p class="text-xl">Productos</p>
 
-                    <div class="w-full bg-slate-100 text-xl flex flex-col items-center">
-                        <table class="w-full bg-purple-500 p-4 rounded-md shadow-md">
-                            <thead class="text-white">
-                                <th class="py-2 px-4 text-center">Producto</th>
+                    <div class=" ScrollFactura w-full h-auto  text-xl flex flex-col items-center">
+                        <table class=" w-full  p-4 rounded-md shadow-md">
+                            <thead class="">
+                                <th class="py-2 px-4 text-center">Nombre</th>
                                 <th class="py-2 px-4 text-center">Cantidad</th>
                                 <th class="py-2 px-4 text-center">Precio</th>
-                                <th class="py-2 px-4 text-center">Subtotales</th>
+                                <th class="py-2 px-4 text-center">Subtotal</th>
                             </thead>
-                            <tbody>
-                                <tr class="bg-amber-200">
+                            
+                                <tbody  >
+                                  
+                                <tr class="bg-slate-50  hover:bg-white ">
                                     <td class="py-2 px-4 text-center">Mouse</td>
                                     <td class="py-2 px-4 text-center">x 4</td>
                                     <td class="py-2 px-4 text-center">35.000</td>
                                     <td class="py-2 px-4 text-center">140.000</td>
                                 </tr>
-                                <tr>
+                                <tr class="bg-slate-50 hover:bg-white ">
                                     <td class="py-2 px-4 text-center">Mouse</td>
                                     <td class="py-2 px-4 text-center">x 4</td>
                                     <td class="py-2 px-4 text-center">35.000</td>
                                     <td class="py-2 px-4 text-center">140.000</td>
                                 </tr>
-                                <tr>
+                                <tr class="bg-slate-50 hover:bg-white ">
+                                    <td class="py-2 px-4 text-center">Mouse </td>
+                                    <td class="py-2 px-4 text-center">x 4</td>
+                                    <td class="py-2 px-4 text-center">35.000</td>
+                                    <td class="py-2 px-4 text-center">140.000</td>
+                                </tr>
+                                <tr class="bg-slate-50 hover:bg-white ">
                                     <td class="py-2 px-4 text-center">Mouse</td>
                                     <td class="py-2 px-4 text-center">x 4</td>
                                     <td class="py-2 px-4 text-center">35.000</td>
                                     <td class="py-2 px-4 text-center">140.000</td>
                                 </tr>
+                                <tr class="bg-slate-50 hover:bg-white ">
+                                    <td class="py-2 px-4 text-center">Mouse</td>
+                                    <td class="py-2 px-4 text-center">x 4</td>
+                                    <td class="py-2 px-4 text-center">35.000</td>
+                                    <td class="py-2 px-4 text-center">140.000</td>
+                                </tr>
+                                <tr class="bg-slate-50">
+                                    <td class="py-2 px-4 text-center">Mouse</td>
+                                    <td class="py-2 px-4 text-center">x 4</td>
+                                    <td class="py-2 px-4 text-center">35.000</td>
+                                    <td class="py-2 px-4 text-center">140.000</td>
+                                </tr>
+                                <tr class="bg-slate-50">
+                                    <td class="py-2 px-4 text-center">Mouse</td>
+                                    <td class="py-2 px-4 text-center">x 4</td>
+                                    <td class="py-2 px-4 text-center">35.000</td>
+                                    <td class="py-2 px-4 text-center">140.000</td>
+                                </tr>
+                                <tr class="bg-slate-50">
+                                    <td class="py-2 px-4 text-center">Mouse</td>
+                                    <td class="py-2 px-4 text-center">x 4</td>
+                                    <td class="py-2 px-4 text-center">35.000</td>
+                                    <td class="py-2 px-4 text-center">140.000</td>
+                                </tr>
+                                <tr class="bg-slate-50">
+                                    <td class="py-2 px-4 text-center">Mouse</td>
+                                    <td class="py-2 px-4 text-center">x 4</td>
+                                    <td class="py-2 px-4 text-center">35.000</td>
+                                    <td class="py-2 px-4 text-center">140.000</td>
+                                </tr>
+                                <tr class="bg-slate-50">
+                                    <td class="py-2 px-4 text-center">Mouse</td>
+                                    <td class="py-2 px-4 text-center">x 4</td>
+                                    <td class="py-2 px-4 text-center">35.000</td>
+                                    <td class="py-2 px-4 text-center">140.000</td>
+                                </tr>
+                                <tr class="bg-slate-50">
+                                    <td class="py-2 px-4 text-center">Mouse</td>
+                                    <td class="py-2 px-4 text-center">x 4</td>
+                                    <td class="py-2 px-4 text-center">35.000</td>
+                                    <td class="py-2 px-4 text-center">140.000</td>
+                                </tr>
+                                <tr class="bg-slate-50">
+                                    <td class="py-2 px-4 text-center">Mouse</td>
+                                    <td class="py-2 px-4 text-center">x 4</td>
+                                    <td class="py-2 px-4 text-center">35.000</td>
+                                    <td class="py-2 px-4 text-center">140.000</td>
+                                </tr>
+                               
+                              
+                                
                             </tbody>
-                            <tfoot>
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td class="bg-sky-400 py-2 px-4 text-center">
-                                        <strong class="bg-sky-200 border-solid border border-t-4 border-t-slate-900 py-2 px-4">420.000</strong>
-                                    </td>
-                                </tr>
-                            </tfoot>
                         </table>
                         
                         
-                        <div class="w-full h-20">
-                            <div class="w-full h-1/2 pl-4 pr-8 flex justify-end items-end bg-sky-400">
-                                <p> Iva-19% : 79.800</p>
-                            </div>
-                            <div class="w-full  h-1/2 pl-4 pr-8 flex justify-end bg-sky-400">
-                                <strong> Total : 499.800</strong>
-                            </div>
-
-                        </div>
                     </div>
+                    <div class=" flex items-start justify-center h-16  ">
+                    
+                        <div class="w-full h-16 pr-10">
+                            <div class="w-full h-full   flex " >
+                                <div class=" text-lg h-auto  flex justify-start items-center  pl-10" style=" width: 55%;">
+                                    <strong> Total : 499.800</strong>
+                                </div>
+                                <div class="  flex flex-col justify-end text-end " style="width: 45%">
+                                    <div>
+                                        <strong class="border-solid  border-b-2 border-b-slate-900  text-center">Subtotal base :  420.000</strong>
+                                    </div>
+                                    <div class=" flex justify-end ">
+                                        <p class="  border-solid border border-b-2 border-b-slate-900"> Iva-19% : 79.800</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+    
+                    </div>
+                    
+                    <div class="flex justify-center text-lg items-end  h-12 ">
+                        <strong>¡ Gracias por su compra !</strong>
+                    </div>
+                    
 
                 </div>
-                <div class="bg-amber-500 flex justify-center ">
-                    <strong>!Gracias por su compra</strong>
-                </div>
+                
+               
                 
             </div>
         </div>
